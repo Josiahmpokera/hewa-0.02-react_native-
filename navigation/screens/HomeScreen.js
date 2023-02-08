68735add41bc70f8ae849416047d9190510045f8
Josiahmpokera <jmpokera@outlook.com>
2023-02-08 06:23:22 +0300

    Refactoring the UI :bug:

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import Ionicons from "react-native-vector-icons/Entypo";
 
 function HomeScreen(navigation) {
@@ -37,10 +37,12 @@ function HomeScreen(navigation) {
           justifyContent: "center",
         }}
       >
+        {/* The Locaiton are Name here */}
         <View
           style={{
             width: "100%",
-            height: 200,
+            padding: 10,
+            height: "auto",
             justifyContent: "center",
             alignItems: "center",
             flexDirection: "row",
@@ -60,6 +62,59 @@ function HomeScreen(navigation) {
             Dar es salaam
           </Text>
         </View>
+
+        {/* End of Locaiton area Element here */}
+
+        {/* The Main View Wather Information Container */}
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              height: "auto",
+              width: "auto",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 85,
+                fontWeight: "800",
+                color: "white",
+              }}
+            >
+              29
+            </Text>
+          </View>
+
+          {/* Another View */}
+          <View
+            style={{
+              height: 10,
+              width: 10,
+              top: -35,
+              left: -18,
+            }}
+          >
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../../assets/snow.png")}
+            />
+          </View>
+
+          {/* Cen Icon */}
+
+          <Text style={{ fontSize: 20, color: "white", top: 25 }}>°C</Text>
+        </View>
+
+        <Text style={{ color: "white", padding: 6 }}>
+          Feels like <Text style={{ color: "orange" }}>149</Text>{" "}
+        </Text>
+        <Text style={{ color: "white" }}>Cloud</Text>
       </View>
     </ImageBackground>
   );
